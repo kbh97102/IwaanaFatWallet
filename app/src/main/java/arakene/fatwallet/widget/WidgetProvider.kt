dc6f07824103df0cfb344723998080cc5c699a83
Arakene <arakene_dev@naver.com>
2021-10-24 12:08:16 +0900

    Update widget

--- app/src/main/java/arakene/fatwallet/widget/WidgetProvider.kt
+++ app/src/main/java/arakene/fatwallet/widget/WidgetProvider.kt
@@ -1,9 +1,23 @@
 package arakene.fatwallet.widget
 
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+
+//https://developer.android.com/guide/topics/appwidgets?hl=ko
 
 class WidgetProvider : AppWidgetProvider() {
 
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
 
+        //Do SomeThing
+
+    }
 
 }
