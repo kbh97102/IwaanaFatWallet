37cf669f89af91f155479905aca7503e59fe6db2
Arakene <arakene_dev@naver.com>
2021-10-24 18:48:34 +0900

    Add firebase save data

--- app/src/main/java/arakene/fatwallet/MainActivity.kt
+++ app/src/main/java/arakene/fatwallet/MainActivity.kt
@@ -1,28 +1,31 @@
 package arakene.fatwallet
 
 import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import arakene.fatwallet.dto.PayDTO
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth : FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
-    private val getLoginResult = registerForActivityResult(
+    private
+    val getLoginResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
-    ){
+    ) {
         if (it.resultCode == Activity.RESULT_OK) {
             val result = Auth.GoogleSignInApi.getSignInResultFromIntent(it.data!!)!!
             if (result.isSuccess) {
@@ -37,17 +40,36 @@ class MainActivity : AppCompatActivity() {
 
 
         auth = FirebaseAuth.getInstance()
+        db = Firebase.firestore
 
-        loginByGoogle()
+        loginByEmail()
+
+        saveData()
+        saveData()
+        saveData()
+        saveData()
+    }
+
+    private var test = 1
+
+    private fun saveData() {
+        db.collection("arakene").document("input").collection("list")
+            .add(PayDTO(PayType.input, "${test++}", 123, "For Test"))
+            .addOnSuccessListener {
+                Toast.makeText(this, "Save Success", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Log.w("SaveError", e)
+            }
     }
 
     private fun loginByEmail() {
-        auth.signInWithEmailAndPassword("test@naver.com", "password").addOnCompleteListener {
-            task ->
-            if (task.isSuccessful) {
-                Toast.makeText(this, "${task.result.user}", Toast.LENGTH_LONG).show()
+        auth.signInWithEmailAndPassword("test@naver.com", "password")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "${task.result.user}", Toast.LENGTH_LONG).show()
+                }
             }
-        }
     }
 
     private fun loginByGoogle() {
@@ -64,8 +86,7 @@ class MainActivity : AppCompatActivity() {
     private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
         val credential = GoogleAuthProvider.getCredential(account?.idToken!!, null)
         auth.signInWithCredential(credential)
-            .addOnCompleteListener {
-                task ->
+            .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     //GoMain
                 }
