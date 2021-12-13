7f88f7f8b7870431c813f6c970e4f8198cb75091
Arakene <arakene_dev@naver.com>
2021-12-13 16:26:22 +0900

    Update Google Login

--- app/src/main/java/arakene/fatwallet/MainActivity.kt
+++ app/src/main/java/arakene/fatwallet/MainActivity.kt
@@ -1,121 +1,66 @@
 package arakene.fatwallet
 
-import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import arakene.fatwallet.databinding.ActivityMainBinding
-import arakene.fatwallet.databinding.MainMenuLayoutBinding
 import arakene.fatwallet.dto.PayDTO
 import arakene.fatwallet.dto.PayType
-import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.formatter.PercentFormatter
-import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding : MainMenuLayoutBinding? = null
+    private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
-
-    private
-    val getLoginResult = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {
-        if (it.resultCode == Activity.RESULT_OK) {
-            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(it.data!!)!!
-            if (result.isSuccess) {
-                firebaseAuthWithGoogle(result.signInAccount)
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val signInIntentForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                try {
+                    val account = task.getResult(ApiException::class.java)
+                    Log.d("TAG", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    Log.w("Error", "Google Sign in failed", e)
+                }
             }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = MainMenuLayoutBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
 
-//
-//        auth = FirebaseAuth.getInstance()
-//        db = Firebase.firestore
-//
-//        loginByEmail()
-//
-//        saveData()
-//        saveData()
-//        saveData()
-//        saveData()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding!!.currentMonthChart.apply {
-            setUsePercentValues(true)
-            description.text = ""
-            isDrawHoleEnabled = false
-            setTouchEnabled(false)
-            setDrawEntryLabels(false)
-            setExtraOffsets(20f, 0f, 20f, 20f)
-            setUsePercentValues(true)
-            isRotationEnabled = false
-            setDrawEntryLabels(false)
-            legend.orientation = Legend.LegendOrientation.VERTICAL
-            legend.isWordWrapEnabled = true
-            legend.textSize = 20f
+        auth = FirebaseAuth.getInstance()
+
+        googleLoginInit()
+        binding.testButton.setOnClickListener {
+            signIn()
         }
-
-        val dataEntries = ArrayList<PieEntry>()
-        dataEntries.add(PieEntry(72f, "Food"))
-        dataEntries.add(PieEntry(26f, "Game"))
-        dataEntries.add(PieEntry(2f, "Other"))
-
-        val colors: ArrayList<Int> = ArrayList()
-        colors.add(Color.parseColor("#4DD0E1"))
-        colors.add(Color.parseColor("#FFF176"))
-        colors.add(Color.parseColor("#FF8A65"))
-
-        val dataSet = PieDataSet(dataEntries, "")
-        val data = PieData(dataSet)
-
-        data.setValueFormatter(PercentFormatter())
-        dataSet.sliceSpace = 3f
-        dataSet.colors = colors
-        binding!!.currentMonthChart.data = data
-        data.setValueTextSize(30f)
-        binding!!.currentMonthChart.apply {
-            setExtraOffsets(5f, 10f, 5f, 5f)
-            animateY(1400, Easing.EaseInOutQuad)
-            holeRadius = 58f
-            transparentCircleRadius = 61f
-            isDrawHoleEnabled = true
-            setHoleColor(Color.WHITE)
-            setDrawCenterText(true)
-            centerText = "Share"
-        }.run { invalidate() }
-
     }
 
-    private fun chartTest() {
-
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            signIn()
+        }
     }
-
-    private var test = 1
 
     private fun saveData() {
         db.collection("arakene").document("input").collection("list")
-            .add(PayDTO(PayType.input, "${test++}", 123, "For Test"))
+            .add(PayDTO(PayType.input, "1", 123, "For Test"))
             .addOnSuccessListener {
                 Toast.makeText(this, "Save Success", Toast.LENGTH_SHORT).show()
             }
@@ -124,32 +69,31 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private fun loginByEmail() {
-        auth.signInWithEmailAndPassword("test@naver.com", "password")
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(this, "${task.result.user}", Toast.LENGTH_LONG).show()
-                }
-            }
-    }
-
-    private fun loginByGoogle() {
-        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+    private fun googleLoginInit() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
-        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
-
-        getLoginResult.launch(googleSignInClient.signInIntent)
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
-    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
-        val credential = GoogleAuthProvider.getCredential(account?.idToken!!, null)
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        signInIntentForResult.launch(signInIntent)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    //GoMain
+                    Log.d("Login", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    Toast.makeText(this, user.toString(), Toast.LENGTH_LONG).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Login", "signInWithCredential:failure", task.exception)
                 }
             }
     }
--- app/src/main/java/arakene/fatwallet/widget/WidgetProvider.kt
+++ app/src/main/java/arakene/fatwallet/widget/WidgetProvider.kt
@@ -9,12 +9,15 @@ import android.content.Intent
 
 class WidgetProvider : AppWidgetProvider() {
 
+
     override fun onUpdate(
         context: Context?,
         appWidgetManager: AppWidgetManager?,
         appWidgetIds: IntArray?
     ) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+
 
         //Do SomeThing
 
